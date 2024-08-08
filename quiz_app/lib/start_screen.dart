9d00683ee60c76a14f96980010e44dfaf936e809
acaciaterra <terra.acacia@gmail.com>
2024-08-08 18:08:56 -0300

    New widgets were created to represent my first screen and the questions screen for the app. Here I've made my first state change to allow a button switch between widgets.

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -37,7 +39,7 @@ class StartScreen extends StatelessWidget {
             padding: EdgeInsets.all(15),
           ),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
               shape: const RoundedRectangleBorder(
